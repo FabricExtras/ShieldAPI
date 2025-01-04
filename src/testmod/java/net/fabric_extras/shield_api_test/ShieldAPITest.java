e24e55db9cf6a4b17b76de6347881d5a0fb6924e
TheRedBrain <thetrueredbrain@gmail.com>
2025-01-04 12:52:51 +0100

    updated test mod

--- src/testmod/java/net/fabric_extras/shield_api_test/ShieldAPITest.java
+++ src/testmod/java/net/fabric_extras/shield_api_test/ShieldAPITest.java
@@ -1,92 +1,79 @@
 package net.fabric_extras.shield_api_test;
 
-import net.fabric_extras.shield_api.item.CustomShieldItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.AttributeModifierSlot;
+import net.minecraft.component.type.AttributeModifiersComponent;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
-import net.minecraft.recipe.Ingredient;
+import net.minecraft.item.ShieldItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.List;
 
 public class ShieldAPITest implements ModInitializer {
 	public static final String MOD_ID = "shield_api_test";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final Item TEST_BUCKLER = new CustomShieldItem(
-			null,
-			() -> Ingredient.ofItems(Items.IRON_INGOT),
-			List.of(
-					new Pair<>(
-							EntityAttributes.GENERIC_ARMOR,
-							new EntityAttributeModifier(
-									Identifier.of(MOD_ID, "shield"),
-									4.0,
-									EntityAttributeModifier.Operation.ADD_VALUE
-							)
+	public static final RegistryKey<Item> TEST_BUCKLER_KEY = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, "test_buckler"));
+	public static final Item TEST_BUCKLER = new ShieldItem(
+			new Item.Settings()
+					.maxDamage(150)
+					.repairable(Items.IRON_INGOT)
+					.attributeModifiers(
+							AttributeModifiersComponent.builder()
+									.add(
+											EntityAttributes.ARMOR,
+											new EntityAttributeModifier(
+													Identifier.of(MOD_ID, "shield"),
+													4.0,
+													EntityAttributeModifier.Operation.ADD_VALUE
+											),
+											AttributeModifierSlot.HAND)
+									.build()
 					)
-			),
-			new Item.Settings().maxDamage(150));
+					.registryKey(TEST_BUCKLER_KEY)
+	);
 
-	public static final Item TEST_SHIELD = new CustomShieldItem(
-			null,
-			() -> Ingredient.ofItems(Items.IRON_INGOT),
-			List.of(
-					new Pair<>(
-							EntityAttributes.GENERIC_ARMOR,
-							new EntityAttributeModifier(
-									Identifier.of(MOD_ID, "shield"),
-									4.0,
-									EntityAttributeModifier.Operation.ADD_VALUE
-							)
+	public static final RegistryKey<Item> TEST_SHIELD_KEY = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, "test_shield"));
+	public static final Item TEST_SHIELD = new ShieldItem(
+			new Item.Settings()
+					.maxDamage(150)
+					.repairable(Items.IRON_INGOT)
+					.equippableUnswappable(EquipmentSlot.OFFHAND)
+					.attributeModifiers(
+							AttributeModifiersComponent.builder()
+									.add(
+											EntityAttributes.ARMOR,
+											new EntityAttributeModifier(
+													Identifier.of(MOD_ID, "shield"),
+													4.0,
+													EntityAttributeModifier.Operation.ADD_VALUE
+											),
+											AttributeModifierSlot.OFFHAND)
+									.build()
 					)
-			),
-			new Item.Settings().maxDamage(150));
+					.registryKey(TEST_SHIELD_KEY)
+	);
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Shield API Test initialized!");
 
+		Registry.register(Registries.ITEM, TEST_BUCKLER_KEY, TEST_BUCKLER);
+		Registry.register(Registries.ITEM, TEST_SHIELD_KEY, TEST_SHIELD);
+
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
 			content.add(TEST_BUCKLER);
 			content.add(TEST_SHIELD);
 		});
-		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "test_buckler"), TEST_BUCKLER);
-		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "test_shield"), TEST_SHIELD);
-
-		((CustomShieldItem) TEST_BUCKLER).setAttributeModifiers(
-				List.of(
-						new Pair<>(
-								EntityAttributes.GENERIC_ARMOR,
-								new EntityAttributeModifier(
-										Identifier.of(MOD_ID, "shield"),
-										2.0,
-										EntityAttributeModifier.Operation.ADD_VALUE
-								)
-						)
-				)
-		);
-
-		((CustomShieldItem) TEST_SHIELD).setAttributeModifiers(
-				List.of(
-						new Pair<>(
-								EntityAttributes.GENERIC_ARMOR,
-								new EntityAttributeModifier(
-										Identifier.of(MOD_ID, "shield"),
-										2.0,
-										EntityAttributeModifier.Operation.ADD_VALUE
-								)
-						)
-				)
-		);
 	}
 }
