ef18f9a8d617836aa2b1b5d458aad8d1aa3f46a5
TheRedBrain <thetrueredbrain@gmail.com>
2026-09-17 20:17:55 +0200

    fixed build script + test mod

--- fabric/src/testmod_fabric/java/net/fabric_extras/shield_api_test/FabricTestMod.java
+++ fabric/src/testmod_fabric/java/net/fabric_extras/shield_api_test/FabricTestMod.java
@@ -1,11 +1,93 @@
 package net.fabric_extras.shield_api_test;
 
+import net.fabric_extras.shield_api.item.CustomShieldItem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public final class FabricTestMod/* implements ModInitializer*/ {
-//	@Override
-//	public void onInitialize() {
-//		// Run our common setup.
-//		ShieldAPITest.init();
-//	}
+import java.util.List;
+
+public final class FabricTestMod implements ModInitializer {
+	public static final String MOD_ID = "shield_api_test";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Shield API Test initialized!");
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
+			content.add(TEST_BUCKLER);
+			content.add(TEST_SHIELD);
+		});
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "test_buckler"), TEST_BUCKLER);
+		Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "test_shield"), TEST_SHIELD);
+
+		((CustomShieldItem) TEST_BUCKLER).setAttributeModifiers(
+				List.of(
+						new Pair<>(
+								EntityAttributes.GENERIC_ARMOR,
+								new EntityAttributeModifier(
+										MOD_ID + "shield",
+										2.0,
+										EntityAttributeModifier.Operation.ADDITION
+								)
+						)
+				)
+		);
+
+		((CustomShieldItem) TEST_SHIELD).setAttributeModifiers(
+				List.of(
+						new Pair<>(
+								EntityAttributes.GENERIC_ARMOR,
+								new EntityAttributeModifier(
+										MOD_ID + "shield",
+										2.0,
+										EntityAttributeModifier.Operation.ADDITION
+								)
+						)
+				)
+		);
+	}
+
+	public static final Item TEST_BUCKLER = new CustomShieldItem(
+			null,
+			() -> Ingredient.ofItems(Items.IRON_INGOT),
+			List.of(
+					new Pair<>(
+							EntityAttributes.GENERIC_ARMOR,
+							new EntityAttributeModifier(
+									MOD_ID + "shield",
+									4.0,
+									EntityAttributeModifier.Operation.ADDITION
+							)
+					)
+			),
+			new Item.Settings().maxDamage(150));
+
+	public static final Item TEST_SHIELD = new CustomShieldItem(
+			null,
+			() -> Ingredient.ofItems(Items.IRON_INGOT),
+			List.of(
+					new Pair<>(
+							EntityAttributes.GENERIC_ARMOR,
+							new EntityAttributeModifier(
+									MOD_ID + "shield",
+									4.0,
+									EntityAttributeModifier.Operation.ADDITION
+							)
+					)
+			),
+			new Item.Settings().maxDamage(150));
+
 }
